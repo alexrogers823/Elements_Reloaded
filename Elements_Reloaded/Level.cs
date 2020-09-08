57f16f95f82e8248b575560be92bda913c162a18
Alex Rogers <alex.rogers823@gmail.com>
2020-09-07 20:09:15 -0400

    added Levels class for gameplay flow

---
+++ Elements_Reloaded/Level.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Elements_Reloaded
+{
+    public class Level
+    {
+        public Level()
+        {
+        }
+
+        public void PlayLevel()
+        {
+        }
+
+        private void _intoduceLevel()
+        {
+        }
+
+        private void _sendBoss()
+        {
+        }
+
+        private void _clearLevel()
+        {
+        }
+    }
+}
