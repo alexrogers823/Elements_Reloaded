cc07348955d25c75248687e8233e896c63496e4d
Alex Rogers <alex.rogers823@gmail.com>
2020-09-13 01:12:26 -0400

    created Gameplay class to handle main flow

---
+++ Elements_Reloaded/Gameplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Elements_Reloaded
+{
+    public class Gameplay
+    {
+        private Hero _hero = GameController.CreateHero();
+
+        public Gameplay()
+        {
+        }
+
+        public void Start()
+        {
+            while (Level.CurrentLevel < 10)
+            {
+               Level.PlayLevel();
+                //shop here
+            }
+        }
+    }
+}
