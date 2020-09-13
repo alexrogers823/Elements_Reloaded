c269951bdcb28da0576551f43f204399efd43eee
Alex Rogers <alex.rogers823@gmail.com>
2020-09-13 01:14:31 -0400

    changed methods to static in Level class

--- Elements_Reloaded/Level.cs
+++ Elements_Reloaded/Level.cs
@@ -3,36 +3,36 @@ namespace Elements_Reloaded
 {
     public class Level
     {
-        private int _currentLevel { get; set; }
+        public static int CurrentLevel { get; set; } = 1;
+        public static int NumberOfEnemies { get; set; } = 5;
 
         public Level()
         {
-            _currentLevel = 1;
         }
 
-        public void PlayLevel()
+        public static void PlayLevel()
         {
             int i = 0;
-            string LevelName = GameDialogue.GetLevelName(_currentLevel);
+            string LevelName = GameDialogue.GetLevelName(CurrentLevel);
 
             _introduceLevel();
 
             Console.WriteLine($"Welcome to the {LevelName}");
 
-            if (_currentLevel == 10)
+            if (CurrentLevel == 10)
             {
                 Console.WriteLine("Final Stage Begin");
             }
             else
             {
-                Console.WriteLine($"Stage {_currentLevel} Begin");
+                Console.WriteLine($"Stage {CurrentLevel} Begin");
             }
 
-            Console.WriteLine("Enemies: 5");
+            Console.WriteLine($"Enemies: {NumberOfEnemies}");
 
             while (i < NumberOfEnemies)
             {
-                _sendEnemy();
+                Enemy enemy = _sendEnemy();
                 // battle enemy
 
                 // check if hero died. if so, end game. If not, increment i
@@ -44,34 +44,37 @@ namespace Elements_Reloaded
             _clearLevel();
         }
 
-        private void _introduceLevel()
+        private static void _introduceLevel()
         {
-            GameDialogue.GetLevelIntro(_currentLevel);
+            GameDialogue.GetLevelIntro(CurrentLevel);
         }
 
-        private void _sendEnemy()
+        private static Enemy _sendEnemy()
         {
+            string EnemyName = GameController.GetEnemyName(elementType, false);
             Console.WriteLine(GameDialogue.GetEnemyIntro());
-            return Enemy(name, elementType);
+            return new Enemy(EnemyName, elementType);
         }
 
-        private void _sendBoss()
+        private static void _sendBoss()
         {
+            string BossName = GameController.GetEnemyName(elementType, true);
             Console.WriteLine($"You did good, but now...here comes the boss, {}!");
-            return Enemy(name, elementType, isBoss);
+            return new Enemy(BossName, elementType, true);
         }
 
-        private void _clearLevel()
+        private static void _clearLevel()
         {
             Console.WriteLine($"{} is defeated! You beat the level!");
             // Coins += 75
             // XP += 50
+            _incrementLevelNumber();
             // clear screen
         }
 
-        private void _incrementLevelNumber()
+        private static void _incrementLevelNumber()
         {
-            _currentLevel++;
+            CurrentLevel++;
         }
 
     }
