c959495e1fc47f86998a6798e72cb06c2a634d1a
Alex Rogers <alex.rogers823@gmail.com>
2020-10-07 19:17:08 -0400

    created Shop class for upgrading in between levels

---
+++ Elements_Reloaded/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Elements_Reloaded
+{
+    public static class Shop
+    {
+        public static void BuyItem()
+        {
+
+        }
+
+        public static void DisplayEligibleContent()
+        {
+
+        }
+
+        public static void ExchangeStone()
+        {
+
+        }
+    }
+}
