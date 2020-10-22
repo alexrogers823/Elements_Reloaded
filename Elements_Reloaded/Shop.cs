c864b204e7f0bebf65b9506aab6cd05154dc3c85
Alex Rogers <alex.rogers823@gmail.com>
2020-10-22 16:34:49 -0400

    set basic skeleton for methods in Shop class

--- Elements_Reloaded/Shop.cs
+++ Elements_Reloaded/Shop.cs
@@ -3,19 +3,21 @@ namespace Elements_Reloaded
 {
     public static class Shop
     {
-        public static void BuyItem()
+        public static void BuyItem(string item)
         {
-
+            // add item to hero inventory
+            Console.WriteLine($"{item.Name} added to inventory!");
         }
 
         public static void DisplayEligibleContent()
         {
-
+            //Iterate through items that hero does not have and are qualified to receive
         }
 
-        public static void ExchangeStone()
+        public static void ExchangeStone(string currentStone, string desiredStone)
         {
-
+            // switch out current stone with desired stone
+            Console.WriteLine($"Now equipped with {desiredStone}!");
         }
     }
 }
